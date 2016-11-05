cbac8f223e107d5daaf5fe219bbd77b70d73d829
JoshTPT <josh@tptech.com.au>
2016-11-06 07:57:42 +1000

    Initial commit

---
+++ tptNetstat/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace tptNetstat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader6,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(637, 238);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "State";
+            this.columnHeader6.Width = 75;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Port";
+            this.columnHeader2.Width = 75;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Service";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Remote IP";
+            this.columnHeader4.Width = 125;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Local IP";
+            this.columnHeader5.Width = 125;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.refreshToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(637, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.refreshToolStripMenuItem.Text = "&Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "&About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(637, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.AutoSize = false;
+            this.toolStripProgressBar1.Maximum = 65535;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(300, 16);
+            this.toolStripProgressBar1.Step = 1;
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(320, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "Doing nothing";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 262);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "tptNetstat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+    }
+}
+
---
+++ tptNetstat/Form1.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Diagnostics;
+using System.Collections;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+
+namespace tptNetstat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<ServiceInfo> services = ReadServicesFile();
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            runNetstat();
+        }
+
+        public void runNetstat()
+        {
+            listView1.Items.Clear();
+            DoubleBuffered = true;
+            toolStripStatusLabel1.Text = "Refreshing...";
+            Thread runner = new Thread(new ThreadStart(runnerThread));
+            runner.Start();
+
+        }
+
+        private void runnerThread()
+        {
+            listView1.BeginUpdate();
+            ManagedIpHelper.UpdateExtendedTcpTable(true, IpHelper.TcpTableType.OwnerPidAll);
+            foreach (TcpRow row in ManagedIpHelper.TcpRows)
+            {
+                string procname;
+                try
+                {
+                    Process process = Process.GetProcessById(row.ProcessId);
+                    if (process.ProcessName != "System") procname = Path.GetFileName(process.MainModule.FileName);
+                    else procname = "System";
+                }
+                catch (Exception ex)
+                {
+                    procname = "Unknown";
+                }
+                ListViewItem itm = new ListViewItem(procname);
+                itm.SubItems.Add(row.State.ToString());
+                string serviceString = "";
+                if (row.State == System.Net.NetworkInformation.TcpState.Listen)
+                {
+                    itm.SubItems.Add(row.LocalEndPoint.Port.ToString());
+                    var svc = services.FirstOrDefault(s => s.Port == row.LocalEndPoint.Port && s.Type.Equals("tcp"));
+                    if (svc != null) serviceString = svc.Name;
+                    else { serviceString = "Unknown"; }
+                }
+                else
+                {
+                    itm.SubItems.Add(row.RemoteEndPoint.Port.ToString());
+                    var svc = services.FirstOrDefault(s => s.Port == row.RemoteEndPoint.Port && s.Type.Equals("tcp"));
+                    if (svc != null) serviceString = svc.Name;
+                    else { serviceString = "Unknown"; }
+                }
+                itm.SubItems.Add(serviceString);
+                itm.SubItems.Add(row.RemoteEndPoint.Address.ToString() + ":" + row.RemoteEndPoint.Port.ToString());
+                itm.SubItems.Add(row.LocalEndPoint.Address.ToString() + ":" + row.LocalEndPoint.Port.ToString());
+                listView1.Items.Add(itm);
+            }
+            toolStripStatusLabel1.Text = "Done";
+            listView1.EndUpdate();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        class ServiceInfo
+        {
+            public ushort Port { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+        }
+
+        static List<ServiceInfo> ReadServicesFile()
+        {
+            var sysFolder = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            if (!sysFolder.EndsWith("\\"))
+                sysFolder += "\\";
+            var svcFileName = sysFolder + "drivers\\etc\\services";
+            var lines = File.ReadAllLines(svcFileName);
+            var result = new List<ServiceInfo>();
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line) || line.StartsWith("#"))
+                    continue;
+                var info = new ServiceInfo();
+                var index = 0;
+                info.Name = line.Substring(index, 16).Trim();
+                index += 16;
+                var temp = line.Substring(index, 9).Trim();
+                var tempSplitted = temp.Split('/');
+                info.Port = ushort.Parse(tempSplitted[0]);
+                info.Type = tempSplitted[1].ToLower();
+                result.Add(info);
+            }
+            return result;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            runNetstat();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            about aboutFrm = new tptNetstat.about();
+            aboutFrm.ShowDialog();
+        }
+    }
+
+#if !MONO
+    #region Managed IP Helper API
+
+    public struct TcpTable : IEnumerable<TcpRow>
+    {
+        #region Private Fields
+
+        private IEnumerable<TcpRow> tcpRows;
+
+        #endregion
+
+        #region Constructors
+
+        public TcpTable(IEnumerable<TcpRow> tcpRows)
+        {
+            this.tcpRows = tcpRows;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public IEnumerable<TcpRow> Rows
+        {
+            get
+            {
+                return this.tcpRows;
+            }
+        }
+
+        #endregion
+
+        #region IEnumerable<TcpRow> Members
+
+        public IEnumerator<TcpRow> GetEnumerator()
+        {
+            return this.tcpRows.GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.tcpRows.GetEnumerator();
+        }
+
+        #endregion
+    }
+
+    public struct TcpRow
+    {
+        #region Private Fields
+
+        private IPEndPoint localEndPoint;
+        private IPEndPoint remoteEndPoint;
+        private TcpState state;
+        private int processId;
+
+        #endregion
+
+        #region Constructors
+
+        public TcpRow(IpHelper.TcpRow tcpRow)
+        {
+            this.state = tcpRow.state;
+            this.processId = tcpRow.owningPid;
+
+            int localPort = (tcpRow.localPort1 << 8) + (tcpRow.localPort2) + (tcpRow.localPort3 << 24) + (tcpRow.localPort4 << 16);
+            long localAddress = tcpRow.localAddr;
+            this.localEndPoint = new IPEndPoint(localAddress, localPort);
+
+            int remotePort = (tcpRow.remotePort1 << 8) + (tcpRow.remotePort2) + (tcpRow.remotePort3 << 24) + (tcpRow.remotePort4 << 16);
+            long remoteAddress = tcpRow.remoteAddr;
+            this.remoteEndPoint = new IPEndPoint(remoteAddress, remotePort);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public IPEndPoint LocalEndPoint
+        {
+            get
+            {
+                return this.localEndPoint;
+            }
+        }
+
+        public IPEndPoint RemoteEndPoint
+        {
+            get
+            {
+                return this.remoteEndPoint;
+            }
+        }
+
+        public TcpState State
+        {
+            get
+            {
+                return this.state;
+            }
+        }
+
+        public int ProcessId
+        {
+            get
+            {
+                return this.processId;
+            }
+        }
+
+        #endregion
+    }
+
+    public static class ManagedIpHelper
+    {
+        public static readonly List<TcpRow> TcpRows = new List<TcpRow>();
+
+        #region Public Methods
+
+        public static void UpdateExtendedTcpTable(bool sorted, IpHelper.TcpTableType tabletype)
+        {
+            TcpRows.Clear();
+
+            IntPtr tcpTable = IntPtr.Zero;
+            int tcpTableLength = 0;
+
+            if (IpHelper.GetExtendedTcpTable(tcpTable, ref tcpTableLength, sorted, IpHelper.AfInet, tabletype, 0) != 0)
+            {
+                try
+                {
+                    tcpTable = Marshal.AllocHGlobal(tcpTableLength);
+                    if (IpHelper.GetExtendedTcpTable(tcpTable, ref tcpTableLength, true, IpHelper.AfInet, tabletype, 0) == 0)
+                    {
+                        IpHelper.TcpTable table = (IpHelper.TcpTable)Marshal.PtrToStructure(tcpTable, typeof(IpHelper.TcpTable));
+
+                        IntPtr rowPtr = (IntPtr)((long)tcpTable + Marshal.SizeOf(table.Length));
+                        for (int i = 0; i < table.Length; ++i)
+                        {
+                            TcpRows.Add(new TcpRow((IpHelper.TcpRow)Marshal.PtrToStructure(rowPtr, typeof(IpHelper.TcpRow))));
+                            rowPtr = (IntPtr)((long)rowPtr + Marshal.SizeOf(typeof(IpHelper.TcpRow)));
+                        }
+                    }
+                }
+                finally
+                {
+                    if (tcpTable != IntPtr.Zero)
+                    {
+                        Marshal.FreeHGlobal(tcpTable);
+                    }
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    #endregion
+
+    #region P/Invoke IP Helper API
+
+    /// <summary>
+    /// <see cref="http://msdn2.microsoft.com/en-us/library/aa366073.aspx"/>
+    /// </summary>
+    public static class IpHelper
+    {
+        #region Public Fields
+
+        public const string DllName = "iphlpapi.dll";
+        public const int AfInet = 2;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// <see cref="http://msdn2.microsoft.com/en-us/library/aa365928.aspx"/>
+        /// </summary>
+        [DllImport(IpHelper.DllName, SetLastError = true)]
+        public static extern uint GetExtendedTcpTable(IntPtr tcpTable, ref int tcpTableLength, bool sort, int ipVersion, TcpTableType tcpTableType, int reserved);
+
+        #endregion
+
+        #region Public Enums
+
+        /// <summary>
+        /// <see cref="http://msdn2.microsoft.com/en-us/library/aa366386.aspx"/>
+        /// </summary>
+        public enum TcpTableType
+        {
+            BasicListener,
+            BasicConnections,
+            BasicAll,
+            OwnerPidListener,
+            OwnerPidConnections,
+            OwnerPidAll,
+            OwnerModuleListener,
+            OwnerModuleConnections,
+            OwnerModuleAll,
+        }
+
+        #endregion
+
+        #region Public Structs
+
+        /// <summary>
+        /// <see cref="http://msdn2.microsoft.com/en-us/library/aa366921.aspx"/>
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TcpTable
+        {
+            public uint Length;
+            public TcpRow row;
+        }
+
+        /// <summary>
+        /// <see cref="http://msdn2.microsoft.com/en-us/library/aa366913.aspx"/>
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TcpRow
+        {
+            public TcpState state;
+            public uint localAddr;
+            public byte localPort1;
+            public byte localPort2;
+            public byte localPort3;
+            public byte localPort4;
+            public uint remoteAddr;
+            public byte remotePort1;
+            public byte remotePort2;
+            public byte remotePort3;
+            public byte remotePort4;
+            public int owningPid;
+        }
+
+        #endregion
+    }
+
+    #endregion
+#endif
+}
